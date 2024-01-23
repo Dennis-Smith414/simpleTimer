0eeba9c558f76c6903dd9f5f91956737e2a09f57
Dennis <dennisnsmith2000@gmail.com>
2024-01-23 17:43:12 -0600

    achived timing function, will work to make more readable

--- src/simpleTimer/Main.java
+++ src/simpleTimer/Main.java
@@ -1,7 +1,28 @@
 package simpleTimer;
+import java.util.concurrent.TimeUnit;
 
 public class Main {
-	public static void main(String args[]) {
+	public static void main(String args[]) throws InterruptedException {
+		boolean x = true;
+		long displayMinutes=0;
+		long startTime = System.currentTimeMillis();
+		System.out.println("Timer: ");
+		
+		while(x) {
+			TimeUnit.SECONDS.sleep(1);
+			 long timePassed = System.currentTimeMillis() - startTime;
+			 long secondsPassed = timePassed/1000;
+			 if(secondsPassed == 60) {
+				 secondsPassed = 0;
+				 startTime = System.currentTimeMillis();
+			 }
+			 if((secondsPassed % 60) == 0) {
+				 displayMinutes++;
+			 }
+		System.out.println(displayMinutes + ": : " + secondsPassed);
+		}
+		
+		
 		
 	}
 
